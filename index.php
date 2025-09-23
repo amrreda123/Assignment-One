f985c8bac985cb5aedf20e6626b93d05914dd7bd
AmrReda <ar524738@gmail.com>
2025-09-23 22:23:59 +0300

    Change after Session

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
-// 1111111111111111111111111111111
+// Ex 1
 echo "<h3 style='color:red;'> One </h3>";
 $name = "Amr Reda";
 echo "Hello, $name";
-// 222222222222222222222222222222
+// Ex 2
 echo "<h3 style='color:red;'> Two </h3>";
 $num1 = 5;
 $num2 = 10;
-echo "Summation This" . "Number One + Number Two = $num1 + $num2 => " . ($num1 + $num2) . "<br>";
-echo "Multiplication This" . "Number One * Number Two = $num1 * $num2 => " . ($num1 * $num2) . "<br>";
-echo "Subtraction This" . "Number One - Number Two = $num1 - $num2 => " . ($num1 - $num2) . "<br>";
-// 333333333333333333333333333333
+ECHO "<H4>";
+echo "Summation This $num1 + $num2 => " . ($num1 + $num2) . "<br>";
+echo "Multiplication This $num1 * $num2 => " . ($num1 * $num2) . "<br>";
+echo "Subtraction This $num1 - $num2 => " . ($num1 - $num2) . "<br>";
+ECHO "</H4>";
+// Ex 3
 echo "<h3 style='color:red;'> Three </h3>";
 $A = 1;
 $B = 2;
@@ -25,20 +27,20 @@ if ($X > 0) {
 } else {
     echo "The Result is Zero";
 }
-// 44444444444444444444444444444444
+// Ex 4
 echo "<h3 style='color:red;'> Four </h3>";
 $N = 13;
 $M = 12;
 echo "Sum = " . $N % 10 . " + " . $M % 10 . " = " . $N % 10 + $M % 10;
-// 55555555555555555555555555555555
+
+// Ex 5
 echo "<h3 style='color:red;'> Five </h3>";
 $NU = 3569;
-if ((int)($NU / 1000) % 2 == 0) {
-    echo "In $NU The First Digit Is " . (int)($NU / 1000) . "and Its Even";
-} else {
-    echo "In $NU The First Digit Is " . (int)($NU / 1000) . " and Its ODD";
-}
-// 666666666666666666666666666
+ECHO ((int)($NU / 1000) % 2 == 0)?
+    "In $NU The First Digit Is " . (int)($NU / 1000) . "and Its Even":
+    "In $NU The First Digit Is " . (int)($NU / 1000) . " and Its ODD";
+
+// EX 6
 echo "<h3 style='color:red;'> Six </h3>";
 $Memo = 15;
 $Moms = 7;
@@ -52,16 +54,10 @@ if ($Memo % $K == 0 && $Moms % $K == 0) {
 } else {
     echo "No One";
 }
-// 7777777777777777777777777
+// Ex 7
 echo "<h3 style='color:red;'> Seven </h3>";
 
 $X = 65;
 $tens = (int)($X / 10);
 $ones = ($X % 10);
-if ($ones != 0 && $tens % $ones == 0) {
-    echo "Yes";
-} elseif ($tens != 0 && $ones % $tens == 0) {
-    echo "Yes";
-} else {
-    echo "No";
-}
+echo (($ones != 0 && $tens % $ones == 0) || ($tens != 0 && $ones % $tens == 0));
